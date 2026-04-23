a643b1572a25c33e7574b74d4bad1134fd4a7b2c
AkshJ2004 <AkshJ2004@users.noreply.github.com>
2026-04-23 18:04:14 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Serif, Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  variable:'--font-ibm-PlexSerif',
+  subsets:['latin'],
+  weight:['400','500','600','700'],
+  display:'swap'  
+})
+
+const monaSans = Mona_Sans({
+ variable:'--font-mona-sans',
+ subsets:['latin'],
+ display:'swap'
+})
+
+export const metadata: Metadata = {
+  title: "Bookified",
+  description: "Transform your books into interactive AI conversations, Upload PDFs, and chat with your book using voice",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans antialiased`}
+    >{children}</body>
+    </html>
+  );
+}
