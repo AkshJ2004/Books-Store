c2beb5dc2646405f46d2cab16127c236fb210632
AkshJ2004 <AkshJ2004@users.noreply.github.com>
2026-04-28 19:45:32 +0530

    fix bugs

--- components/VapiControls.tsx
+++ components/VapiControls.tsx
@@ -11,7 +11,7 @@ import {useRouter} from "next/navigation";
 import {useEffect} from "react";
 
 const VapiControls = ({ book }: { book: IBook }) => {
-    const { status, isActive, messages, currentMessage, currentUserMessage, duration, start, stop, clearError, limitError, isBillingError, maxDurationSeconds } = useVapi(book)
+    const { status, isActive, messages, currentMessage, currentUserMessage, duration, start, stop, clearError, limitError, isBillingError } = useVapi(book)
     const router = useRouter();
 
     useEffect(() => {
@@ -97,7 +97,7 @@ const VapiControls = ({ book }: { book: IBook }) => {
 
                             <div className="vapi-status-indicator">
                                 <span className="vapi-status-text">
-                                    {formatDuration(duration)}/{formatDuration(maxDurationSeconds)}
+                                    
                                 </span>
                             </div>
                         </div>
