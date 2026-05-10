b38a999df7d6fbfecb15b90f3fa99c59ce8af42a
AkshJ2004 <AkshJ2004@users.noreply.github.com>
2026-05-10 12:09:54 +0530

    fix clerk middleware

--- proxy.ts
+++ proxy.ts
@@ -1,23 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/vapi/search-book(.*)",
-  "/api/upload(.*)",
+const isProtectedRoute = createRouteMatcher([
+  "/profile(.*)",
+  "/my-profile(.*)",
+  "/api(.*)"
 ]);
 
-// Next.js 16: file must be proxy.ts, export must be named "proxy"
-export const proxy = clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/((?!_next|.*\\..*).*)",
+    "/",
+    "/(api|trpc)(.*)"
   ],
 };
